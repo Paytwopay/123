a38914b5a1e03c0f46fc1fa71caaffb7b609a8f4
ansouling <40800958+ansouling@users.noreply.github.com>
2018-07-08 18:43:20 +0700

    Update config.py

--- LINEPY/config.py
+++ LINEPY/config.py
@@ -27,12 +27,12 @@ class Config(object):
         'LINE_SERVICES': '1459630796'
     }
 
-    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[96]
-    APP_VER     = '8.1.1 DESKTOP:WIN:5.1.2600-XP-x64(5.5.0.1585)'
+    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[369]
+    APP_VER     = '"CHROMEOS\t1.4.17\tChrome_OS\t1"'
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = '5.5.0.1585'
-    SYSTEM_VER  = 'WINDOWSt5.1.2600-XP-x64'
-    SYSTEM_VER  = '11.2.5'
+    SYSTEM_VER  = '"CHROMEOS\t1.4.17\tChrome_OS\t1"'
+    SYSTEM_VER  = '"Line/1.4.17"'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
